610175b4d726b9a6705f51b1fb5628a79114dc3e
Aniket Bhatia <aniketbhatia9@gmail.com>
2025-05-17 10:13:14 +0530

    NewsApp Complete Project

--- src/Components/spinner.js
+++ src/Components/spinner.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
 import loading from './loading.gif';
 
-export class spinner extends Component {
-  render() {
+const Spinner = (props) => {
+  
     return (
       <div className='text-center my-3'> 
         <img src={loading} alt="Loading" style={{ width: "100px", height: "100px", margin: "auto", display: "block" }} />
       </div>
     );
   }
-}
 
-export default spinner;
+
+export default Spinner;
